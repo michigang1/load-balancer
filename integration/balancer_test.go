166e6b2c29b3d1acbcd8872a69ffff8e9eb26d56
chebelok <lotokhinivan@gmail.com>
2023-06-02 22:34:35 +0300

    added integration tests

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -2,6 +2,7 @@ package integration
 
 import (
 	"fmt"
+	"gopkg.in/check.v1"
 	"net/http"
 	"os"
 	"testing"
@@ -14,17 +15,41 @@ var client = http.Client{
 	Timeout: 3 * time.Second,
 }
 
-func TestBalancer(t *testing.T) {
+type BalancerIntegrationSuite struct{}
+
+func init() {
+	check.Suite(&BalancerIntegrationSuite{})
+}
+
+func (b *BalancerIntegrationSuite) TestBalancer(c *check.C) {
 	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
-		t.Skip("Integration test is not enabled")
+		c.Skip("Integration test is not enabled")
 	}
 
-	// TODO: Реалізуйте інтеграційний тест для балансувальникка.
-	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
-	if err != nil {
-		t.Error(err)
+	testCases := []struct {
+		endpoint string
+		expected string
+	}{
+		{"/api/v1/some-data1", "server1:8080"},
+		{"/api/v1/some-data2", "server2:8080"},
+		{"/api/v1/some-dat3", "server3:8080"},
+		{"/api/v1/some-data4", "server2:8080"},
 	}
-	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+
+	for _, tc := range testCases {
+		resp, err := client.Get(fmt.Sprintf("%s%s", baseAddress, tc.endpoint))
+		if err != nil {
+			c.Error(err)
+		}
+		c.Check(resp.Header.Get("lb-from"), check.Equals, tc.expected)
+	}
+
+	//// TODO: Реалізуйте інтеграційний тест для балансувальникка.
+	//resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+	//if err != nil {
+	//	c.Error(err)
+	//}
+	//c.Logf("response from [%s]", resp.Header.Get("lb-from"))
 }
 
 func BenchmarkBalancer(b *testing.B) {
