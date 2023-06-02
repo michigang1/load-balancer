2517caee6975f171cafd86ca079de7e7de97af12
Michael Chirozidi <maikl.tigriv@gmail.com>
2023-06-02 18:35:19 +0200

    Balancer is finally optimized for tests

--- cmd/lb/balancer.go
+++ cmd/lb/balancer.go
@@ -4,14 +4,13 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"github.com/roman-mazur/design-practice-2-template/httptools"
+	"github.com/roman-mazur/design-practice-2-template/signal"
 	"hash/fnv"
 	"io"
 	"log"
 	"net/http"
 	"time"
-
-	"github.com/roman-mazur/design-practice-2-template/httptools"
-	"github.com/roman-mazur/design-practice-2-template/signal"
 )
 
 var (
@@ -87,78 +86,73 @@ func forward(dst string, rw http.ResponseWriter, r *http.Request) error {
 }
 
 type LoadBalancer struct {
-	serversPool    []string
-	healthyServers []string
-	traceEnabled   *bool
-	port           *int
+	serversHealthyStatus map[string]bool
+	durationOfChecking   time.Duration
 }
 
-// NewLoadBalancer creates a new instance of LoadBalancer.
-func NewLoadBalancer(serversPool []string, traceEnabled *bool, port *int) *LoadBalancer {
-	return &LoadBalancer{
-		serversPool:    serversPool,
-		healthyServers: []string{},
-		traceEnabled:   traceEnabled,
-		port:           port,
-	}
-}
-
-// healthCheck periodically checks the health of each server in the serversPool and updates the healthyServers list accordingly.
-func (lb *LoadBalancer) healthCheck() {
-	for i, server := range lb.serversPool {
+// CheckHealthyServers periodically checks the health of each server in the serversPool pool
+func (lb *LoadBalancer) CheckHealthyServers() {
+	timeCheck := time.Tick(lb.durationOfChecking)
+	for i, server := range serversPool {
 		go func(server string, i int) {
-			for range time.Tick(10 * time.Second) {
-				isHealthy := health(server)
-				if !isHealthy {
-					lb.serversPool[i] = ""
+			for range timeCheck {
+				if health(server) {
+					lb.serversHealthyStatus[server] = true
 				} else {
-					lb.serversPool[i] = server
+					lb.serversHealthyStatus[server] = false
 				}
-
-				lb.healthyServers = lb.healthyServers[:0]
-
-				for _, value := range lb.serversPool {
-					if value != "" {
-						lb.healthyServers = append(lb.healthyServers, value)
-					}
-				}
-
-				log.Println(server, isHealthy)
 			}
 		}(server, i)
 	}
 }
 
-// selectServer selects a server based on the remote address and the list of healthy servers.
-func (lb *LoadBalancer) selectServer(remoteAddr string) string {
-	hash := fnv.New32a()
-	_, _ = hash.Write([]byte(remoteAddr))
-	sum := int(hash.Sum32())
-	index := sum % len(lb.healthyServers)
-	log.Println("hash", sum)
-	log.Println("selected server", index)
-	return lb.healthyServers[index]
+// GetHealthyServers  checks the health of each server in the serversHealthyStatus and updates the healthyServers list accordingly.
+func (lb *LoadBalancer) GetHealthyServers() []string {
+	var healthyServers []string
+	for _, server := range serversPool {
+		if lb.serversHealthyStatus[server] {
+			healthyServers = append(healthyServers, server)
+		}
+	}
+	return healthyServers
 }
 
-// Run starts the load balancer and handles incoming requests.
-func (lb *LoadBalancer) Run() {
-	lb.healthCheck()
-
-	frontend := httptools.CreateServer(*lb.port, http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		remoteAddr := r.RemoteAddr
-		server := lb.selectServer(remoteAddr)
-		forward(server, rw, r)
-	}))
-
-	log.Println("Starting load balancer...")
-	log.Printf("Tracing support enabled: %t", *lb.traceEnabled)
-	frontend.Start()
-	signal.WaitForTerminationSignal()
+// SelectServer selects a server based on the remote address and the list of healthy servers.
+func (lb *LoadBalancer) SelectServer(remoteAddr string) string {
+	healthyServers = lb.GetHealthyServers()
+	if len(healthyServers) == 0 {
+		log.Println("No healthy servers")
+		return ""
+	} else {
+		serverIndex := hash(remoteAddr)
+		return healthyServers[serverIndex]
+	}
+}
+func hash(input string) int {
+	hash := fnv.New32a()
+	_, _ = hash.Write([]byte(input))
+	sum := int(hash.Sum32())
+	index := sum % len(input)
+	log.Println("hash", sum)
+	log.Println("selected server", index)
+	return index
 }
 
 func main() {
 	flag.Parse()
 
-	lb := NewLoadBalancer(serversPool, traceEnabled, port)
-	lb.Run()
+	balancer := &LoadBalancer{}
+	balancer.serversHealthyStatus = make(map[string]bool)
+	balancer.durationOfChecking = 10 * time.Second
+
+	go balancer.CheckHealthyServers()
+	frontend := httptools.CreateServer(*port, http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		server := balancer.SelectServer(r.RemoteAddr)
+		forward(server, rw, r)
+	}))
+
+	log.Println("Starting load balancer...")
+	log.Printf("Tracing support enabled: %t", *traceEnabled)
+	frontend.Start()
+	signal.WaitForTerminationSignal()
 }
